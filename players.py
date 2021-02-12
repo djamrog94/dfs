c7a492033a45699ca7aea9958d8c8fac4ab7e360
david <jamrog.D@northeastern>
2021-02-11 21:53:55 -0500

    at bat parse

--- players.py
+++ players.py
@@ -29,6 +29,10 @@ class Player():
     def get_position(self):
         pass
 
+
+
+class Pitcher(Player):
+
     def get_era(self, date, num_games=None, start_date=None):
         '''
         date in string form yyyy-mm-dd
@@ -46,7 +50,8 @@ class Player():
         cur = conn.cursor()
         cur.execute(f"SELECT pitch_seq_tx, event_tx from event where game_id IN \
             (SELECT game_id from game where game_dt <= {d} and (away_start_pit_id='{pid}' or home_start_pit_id='{pid}') ORDER BY game_dt DESC LIMIT {num_games}) ")
-        df = pd.Dataframe(cur.fetchall())
+        data = cur.fetchall()
+        at_bats = [At_bat(x) for x in data]
         print('hi')
 
 
@@ -84,7 +89,8 @@ class At_bat():
     hit_map = {'S': 'single',
                     'D': 'double',
                     'T': 'triple',
-                    'H': 'home run'}
+                    'H': 'home run',
+                    'W': 'walk', 'B': 'balk', 'K': 'strikeout'}
     hit_modfier_map = {'+': 'hard', '-': 'soft'}
     position_map = {'1': 'P', '2': 'C', '3': '1B',
                     '4': '2B', '5': '3B', '6': 'SS',
@@ -92,35 +98,84 @@ class At_bat():
     location_map = {}
 
 
-    def __init__(self, game_id, bat_id):
+    def __init__(self, data):
 
-        # self.type_hit = 0
-        # self.location = 0
-        # self.rbi = 0
-        # # true if L/R or R/L
-        # self.split = False
-        # self.event = # get the id
-        # self.pitches = # get the pitches
-        # self.
-        # self.num_pitches = len(self.pitches)
-        self.game_id = game_id
-        self.bat_id = bat_id
-        self.pitches = ''
+        self.pitches = data[0]
+        self.event = data[1]
+        # pitches
+        self.balls = 0
+        self.strikes = 0
+        self.called = 0
+        self.swing = 0
+        self.fouls = 0
+        self.num_pitches = len(self.pitches)
+        # hits
+        self.type_hit = 0
+        self.location = 0
+        self.bases = -1
+        self.hard = 0
+        # other
+        self.rbi = 0
+        # true if L/R or R/L
+        self.split = False
 
-    def _parse(self):
-        pass
+        self.parse_pitches()
+        self.parse_event()
 
+    def parse_pitches(self):
+        c = Counter(self.pitches)
+        for i in c.keys():
+            # ball
+            if i in ['B', 'I', 'P']:
+                self.balls += c[i]
+            # strike
+            if i in ['C', 'K', 'L', 'M', 'O', 'Q', 'R', 'S']:
+                self.strikes += c[i]
+                if i =='C':
+                    self.called += c[i]
+                elif i == 'S':
+                    self.swing += c[i]
+            # foul
+            if i in ['F', 'T']:
+                self.fouls += c[i]
 
+    def parse_event(self):
+        e = self.event.split('/')
+        if len(e) == 1:
+            self.bases = e[0].split('.')[0]
+            return
+        play = e[0].split('.')[0]
+        modifier = e[1]
+        runners = e[-1].split('.')[-1]
+        if play[0] in self.hit_map.keys():
+            self.bases = play[0]
+        else:
+            self.bases = 'O'
+        self.type_hit = modifier[0]
+        idx = 1
+        while idx < len(modifier):
+            if modifier[idx] == '+':
+                self.hard = 1
+                break
+            elif modifier[idx] == '-':
+                self.hard = -1
+                break
+            if modifier[idx] not in ['/', '.']:
+                idx += 1
+            else:
+                break
 
+        self.location = modifier[1:idx]
+        c = Counter(runners)
+        try:
+            self.rbi += c['H']
+        except:
+            pass
+            
+        
 
     def is_gb(self):
         pass
-
-
-
-
-
-
 
 class Slate_player():
     def __init__(self, player):
@@ -129,7 +184,7 @@ class Slate_player():
 
 
 def main():
-    hi = Player('Aaron Nola')
+    hi = Pitcher('Aaron Nola')
     hi.get_era('2020-09-27', 5)
     print('test')
 
